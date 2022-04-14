df3fdcf41130b81f13696d63f1f0bde010126a2f
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-04-13 20:18:31 -0600

    add annotations/comments

--- variables.rb
+++ variables.rb
@@ -1,15 +1,19 @@
 first_name = "Sandy"
 middle_name = "Marie"
 birth_month = "December"
+#vars for strings covering first and middle name, along with birth month
 
 current_age = 32
 years_in_career = 0
 years_in_school = 8
+#vars for integers covering current age, years in career, and years in school
 
 hs_gpa = 3.5
 college_gpa = 3.0
 current_gpa = 4.0
+#vars for floats covering highschool, college, current gpa's
 
 graduated_highschool = true
 graduated_firstcollege = true
 graduated_turing = false
+#vars for booleans covering if graduated highschool, my first college, and current turing
